90c309190f787bbeb7f157da48457a787acaeb5a
BeeSwap01 <101956590+BeeSwap01@users.noreply.github.com>
2022-03-29 19:55:20 +0700

    Create troll.js

---
+++ troll.js
@@ -0,0 +1,11 @@
+let btn = document.createElement("button");
+
+btn.innerHTML = "troll";
+
+btn.onclick = function () {
+   
+  alert("U are Stupid muahhahaha");
+
+};
+
+document.body.appendChild(btn);
